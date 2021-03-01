81171e86f567a7e30d26bddd8ef3d857e64fd345
John Varadi <jvaradi329@gmail.com>
2021-02-28 20:06:51 -0800

    updated aboutMe page and added resume

--- src/AboutMe/aboutMe.js
+++ src/AboutMe/aboutMe.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import manSit from "./manOnTable.svg";
+import resume from "./Resume.pdf";
 import "./aboutMe.css";
 import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -14,20 +15,20 @@ class AboutMe extends Component {
           <div className="short-bio-text">
             <h1>Hi I'm John.</h1>
             <p>
-              I am currently a Senior at Queens College in NYC. I was born and
-              rasied in Queens. I'm a passionate Software Developer 🚀 having
-              experience with building Web and Mobile applications with
-              JavaScript / React / Python / Django and some other cool libraries
-              and frameworks
+              I am currently studying computer science at Queens College in NYC. I was born and
+              raised in Queens. I'm a passionate Software Developer 🚀 
+              experienced with databases, interface development, healthcare IT and programming languages
+              including JavaScript / React / Python / Django / Java / SQL and some
+              other cool libraries and frameworks
             </p>
             <Button
               // insert your own resume pdf link in href
-              href="https://www.freecodecamp.org/news/content/images/2020/03/1_software_resume_tk-1.jpg"
+              href={resume}
             >
               See My Resume
             </Button>
             <div className="dev-icons">
-              <p className="dev-icons-subTitle">Check Out My Dope Tech Stack</p>
+              <p className="dev-icons-subTitle">Check Out My Tech Stack</p>
               <FaHtml5 />
               <FaCss3Alt />
               <FaPython />
